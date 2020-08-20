22475a976988f658269aee8231cec37f7d02dca4
Matheus Adler <Adler@imac-robert.mirante.com.br>
2020-08-20 12:59:46 -0300

    start project

---
+++ resources/views/admin/pages/products/index.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Gestão de Produtos')
+
+@section('content')
+
+<h1>Exibindo Pordutos</h1>
+
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.styles(['./node_modules/bootstrap/dist/css/bootstrap.css',
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './node_modules/sweetalert2/dist/sweetalert2.min.css',
+    'resources/css/app.css'
+], 'public/css/app.css');
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.scripts(['./node_modules/jquery/dist/jquery.min.js',
+    './node_modules/popper.js/dist/umd/popper.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/moment/min/moment.min.js',
+    './node_modules/sweetalert2/dist/sweetalert2.min.js',
+    'resources/js/app.js'
+], 'public/js/app.js');
